b92fead110304be2d4055323a25eeb876ea23c9c
Théo BARCELO <144953876+BARCELO-Theo-2326015@users.noreply.github.com>
2023-10-09 17:06:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ void renderMap(vector<vector<unsigned>> table) {
     }
 }
 
-void createMap(vector<vector<unsigned>> table, unsigned nbColumn, vector<vector<unsigned>> snakeWhere) {
+vector<vector<unsigned>> createMap(vector<vector<unsigned>> table, unsigned nbColumn, vector<vector<unsigned>> snakeWhere) {
     // création des différentes cases verticales du tableau
     for(unsigned i = 0; i < nbColumn; ++i) {
         // creation d'un tableau par case pour cases horizontales
@@ -41,16 +41,24 @@ void createMap(vector<vector<unsigned>> table, unsigned nbColumn, vector<vector<
         // création des différentes cases horizontales du nouveau tableau
         for(unsigned j = 0; j < nbColumn; ++j) {
             // ajout de la case verticale
-            if(j == snakeWhere[0][0] && i == snakeWhere[0][1]) newTable.push_back(2);
-            else newTable.push_back(1);
+            bool placed = false;
+            for(unsigned o = 0; o < snakeWhere.size(); ++o) {
+                if(j == snakeWhere[o][0] && i == snakeWhere[o][1]) {
+                    newTable.push_back(2);
+                    placed = true;
+                }
+            }
+            if(placed == false) newTable.push_back(1);
         }
 
         // ajout de la table verticale
         table.push_back(newTable);
     }
+    // return
+    return table;
 }
 
-void changeSnakePosition(unsigned snakeWhat, vector<vector<unsigned>> snakeWhere) {
+vector<vector<unsigned>> changeSnakePosition(unsigned snakeWhat, vector<vector<unsigned>> snakeWhere) {
     vector<unsigned> newPos = snakeWhere[0];
     switch(snakeWhat)
     {
@@ -78,25 +86,31 @@ void changeSnakePosition(unsigned snakeWhat, vector<vector<unsigned>> snakeWhere
             break;
     }
     // insertion de la nouvelle position
-    snakeWhere.insert(snakeWhere.begin(), snakeWhere[0]);
+    snakeWhere.insert(snakeWhere.begin(), newPos);
     // supression de la dernière position
     snakeWhere.resize(snakeWhere.size()-1);
+    // return
+    return snakeWhere;
 }
 
 int main()
 {
     // affichage de titre
-    cout << "Bienvenue dans le Snake (version mini)";
+    cout << "Bienvenue dans le Snake (version mini)" << endl;
     sleep_for(seconds(1));
-    cout << 3;
+    cout << 3 << endl;
     sleep_for(seconds(1));
-    cout << 2;
+    cout << 2 << endl;
     sleep_for(seconds(1));
-    cout << 1;
+    cout << 1 << endl;
     sleep_for(seconds(1));
+
 
     // nombre de colomnes a définir
     unsigned nbColumn = 8;
+
+    // c fini ?
+    bool finished = false;
 
     // variable du déplacement du snake
     unsigned snakeWhat = 1; // 1 = gauche, 2 = droite, 3 = haut, 4 = bas
@@ -108,20 +122,26 @@ int main()
     vector<vector<unsigned>> table;
 
     // création de la map
-    createMap(table, nbColumn, snakeWhere);
+    table = createMap(table, nbColumn, snakeWhere);
 
     // render original
     renderMap(table);
 
     // boucle
     while(true) {
-        sleep_for(nanoseconds(500));
+        if(finished == true) {
+            cout << "Vous avez perdu !" << endl;
+            break;
+        }
+
+        cin >> snakeWhat;
+        //sleep_for(nanoseconds(500));
 
         // modification de la position du snake
-        changeSnakePosition(snakeWhat, snakeWhere);
+        snakeWhere = changeSnakePosition(snakeWhat, snakeWhere);
 
         // changement de la map
-        createMap(table, nbColumn, snakeWhere);
+        table = createMap(table, nbColumn, snakeWhere);
 
         // render de la map
         renderMap(table);
